754c4a06a4eef6f2c9c0f9a22cd78e77d23a60fd
angelcom <179908080>
2021-04-15 12:29:07 +0800

    First Commit2

--- main.ts
+++ main.ts
@@ -247,7 +247,7 @@ namespace knock_robot_neopixel {
 
     //% blockId=knock_robot_neopixel_getLedPlots
     //% block="读取led5*5状态，按位组成一个整数返回"
-    export function getLedPlots(): number {
+    export function getLedPlots(): string {
         let plots = 0;
         for (let i = 0; i < 5; i++) {
             for (let j = 0; j < 5; j++) {
@@ -257,7 +257,7 @@ namespace knock_robot_neopixel {
                 }
             }
         }
-        return plots;
+        return plots.toString();
     }
 
     function ledOnBoard(msg: string) {
@@ -265,7 +265,7 @@ namespace knock_robot_neopixel {
         let arg = msg.substr(3, msg.length - 3);
         switch (cmd) {
             case "llp": // 读取板载led5*5状态
-                bluetooth.uartWriteString("llp" + getLedPlots().toString())
+                bluetooth.uartWriteString("llp" + getLedPlots())
                 break;
             case "slp":   // 设置板载led一点
                 let x = parseInt(arg.substr(0, 1))
@@ -277,7 +277,7 @@ namespace knock_robot_neopixel {
                 else {
                     led.unplot(x, y); // 关闭
                 }
-                bluetooth.uartWriteString("llp" + getLedPlots().toString())
+                bluetooth.uartWriteString("llp" + getLedPlots())
                 break;
         }
     }
